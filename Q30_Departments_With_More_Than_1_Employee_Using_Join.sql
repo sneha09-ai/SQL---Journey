672bb2a7ecafdd436bc7f3c9b1564f53db7bef33
sneha sharma <ss2272736@gmail.com>
2026-06-25 03:54:00 +0530

    Create Q30_Departments_With_More_Than_1_Employee_Using_Join.sql

---
+++ Q30_Departments_With_More_Than_1_Employee_Using_Join.sql
@@ -0,0 +1,5 @@
+SELECT Departments.DeptName,COUNT(*) AS "Total Employees" 
+FROM Departments
+JOIN Employees ON Departments.DeptID=Employees.DeptID
+GROUP BY Departments.DeptName
+HAVING COUNT(*)>1;
