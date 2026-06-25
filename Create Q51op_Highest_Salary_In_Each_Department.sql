a382a4b8cd0ff576073a81c4738b218ec116eafb
sneha sharma <ss2272736@gmail.com>
2026-06-25 19:54:57 +0530

    Create  Create Q51op_Highest_Salary_In_Each_Department.sql

---
+++ Create Q51op_Highest_Salary_In_Each_Department.sql
@@ -0,0 +1,14 @@
+SELECT Name, DeptName, Salary
+FROM (
+    SELECT Employees.Name,
+           Departments.DeptName,
+           Employees.Salary,
+           ROW_NUMBER() OVER (
+               PARTITION BY Departments.DeptName
+               ORDER BY Employees.Salary DESC
+           ) AS RowNo
+    FROM Employees
+    JOIN Departments
+    ON Employees.DeptID = Departments.DeptID
+) AS T
+WHERE RowNo =1;
